f55e554b4fd692e4294c00ff1f286bb7a5b316ea
HERB Geoffroy <geoffroy.herb@hotmail.fr>
2024-04-09 11:09:46 +0200

    complete integration tests

---
+++ tests/TrollIt.Api.Tests/Mock/MockCookieAuthenticationHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace TrollIt.Api.Tests.Mock;
+
+public class MockAuthenticationHandler(
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder,
+    IHttpContextAccessor contextAccessor,
+    MockAuthenticatedUsersRepository mockAuthenticatedUsersRepository
+    ) : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder)
+{
+    public const string SchemeName = "MockAuthentication";
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        if (contextAccessor.HttpContext?.Request.Headers.TryGetValue("Mock-Authenticated-UserId", out var users) ?? false)
+        {
+            var user = users.FirstOrDefault();
+            if (user != null && Guid.TryParse(user, out var userId) && mockAuthenticatedUsersRepository.IsUserAuthenticated(userId))
+            {
+                var claims = new[] { new Claim(ClaimTypes.Name, userId.ToString()) };
+                var identity = new ClaimsIdentity(claims, SchemeName);
+                var principal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principal, SchemeName);
+
+                var result = AuthenticateResult.Success(ticket);
+
+                return Task.FromResult(result);
+            }
+        }
+
+        return Task.FromResult(AuthenticateResult.Fail("No user authenticated"));
+    }
+}
---
+++ tests/TrollIt.Api.Tests/Mock/MockCookieExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TrollIt.Api.Tests.Mock;
+
+public static class MockCookieExtensions
+{
+    public static void AddMockCookieAuthentication(this IServiceCollection services, MockAuthenticatedUsersRepository mockAuthenticatedUsersRepository)
+    {
+        services.AddHttpContextAccessor();
+        services.AddSingleton(mockAuthenticatedUsersRepository);
+        services.AddTransient<MockAuthenticationHandler>();
+        
+        services.AddAuthentication(MockAuthenticationHandler.SchemeName)
+            .AddScheme<AuthenticationSchemeOptions, MockAuthenticationHandler>(MockAuthenticationHandler.SchemeName, options => { });
+    }
+}
